b013497175d94a89cdac81fbb8a0b58f914f374c
User <rasulov_aziret@mail.ru>
2022-02-17 19:55:54 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int a, b, i, j, flag;
+
+
+            System.out.printf("Enter interval: ");
+            a = sc.nextInt();
+
+
+            System.out.printf("\nEnter interval: ");
+            b = sc.nextInt();
+
+
+            System.out.printf("\nPrime  are: ", a, b);
+
+
+            for (i = a; i <= b; i++) {
+
+
+                if (i == 1 || i == 0)
+                    continue;
+
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j) {
+                    if (i % j == 0) {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if (flag == 1)
+                    System.out.println(i);
+            }
+        }
+        }
+
